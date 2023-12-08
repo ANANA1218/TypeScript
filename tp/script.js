05d589146860b5be319103ce9bc290bcaebabbc7
Annabelle <annabelle.bosse.pro@gmail.com>
2023-12-08 15:06:03 +0100

    filtre

--- tp/script.js
+++ tp/script.js
@@ -28,5 +28,33 @@ function fetchUsers() {
         .catch(function (error) {
         console.error('Erreur lors de la récupération des utilisateurs :', error);
     });
+    var userSearchInput = document.getElementById('userSearch');
+    userSearchInput.addEventListener('input', function () {
+        var searchTerm = userSearchInput.value.toLowerCase();
+        var userItems = document.querySelectorAll('#userList > li');
+        userItems.forEach(function (userItem) {
+            var userName = userItem.textContent.toLowerCase();
+            if (userName.includes(searchTerm)) {
+                userItem.style.display = 'block';
+            }
+            else {
+                userItem.style.display = 'none';
+            }
+        });
+    });
+    var postSearchInput = document.getElementById('postSearch');
+    postSearchInput.addEventListener('input', function () {
+        var searchTerm = postSearchInput.value.toLowerCase();
+        var postItems = document.querySelectorAll('#userList li ul li');
+        postItems.forEach(function (postItem) {
+            var postTitle = postItem.textContent.toLowerCase();
+            if (postTitle.includes(searchTerm)) {
+                postItem.style.display = 'block';
+            }
+            else {
+                postItem.style.display = 'none';
+            }
+        });
+    });
 }
 document.addEventListener('DOMContentLoaded', fetchUsers);
